8e49f63c90c3a9d3196c9dd9929bf7c9d48447f9
CameIIian <90742148+CameIIian@users.noreply.github.com>
2025-03-30 00:38:00 +0900

    ⚡️Update vrc_world_user_checker.py
    1. 不要なimport文の削除
    2. 出力ファイル名に日付情報を追加
    3. 引数の個数を事前に判断するように変更
    4. 3より、複数のログファイルを一度に変換できるように変更
    5. その他表示面の強化

--- vrc_world_user_checker.py
+++ vrc_world_user_checker.py
@@ -1,49 +1,77 @@
 # VRChat Log  Formatting Tool
 # The original code was created by sunasaji.
-# Forked by Camellian
+# Forked by CameIIian
 
-# Changes from original code
+# Changes from original code:
 # 1. Remove unnecessary import statements
 # 2. Fixing regular expressions used to retrieve user information
 #    "Player" -> "Behaviour"
+# 3. Display concise usage in case of error
+# 4. Check the number of command line args
+# 5. Adding input file date information to output files
+# 6. Convert multiple log files in 1 batch
+
+# TODO:
+# - Improved method of "obtaining/determining the date" of the input log file, which is added to the formatted log file.
 
 # imports
-import sys
-import re
+from sys import argv, exit
+from re import search
 from os.path import exists
 
+# Format command line arguments
+def getArgs():
+    # error cases
+    if len(argv) < 2:
+        print('[NG] Too few arguments. Enter 1 log file you wish to convert after the conversion script')
+        exit(1)
+    
+    # ok case
+    return argv[1::]
+
 # Main function to format data
-def main():
-    # Target files for the 1st argument
-    path = sys.argv[1] 
+def main(args):
+    print("args: "+str(args))
+    # Run until all files are formatted
+    for loop in args:
+        print("loop: "+loop)
+        # Target files for the 1st argument
+        path = loop
 
-    # Don't run if file not found
-    if not exists(path):
-        print('{} is not found'.format(path))
-        return 
-	
-    # Determine output file name
-    output_file = open("VRChat_usrlog.txt", mode='a', encoding='utf-8', errors='ignore', buffering=1)
+        # Don't run if file not found
+        if not exists(path):
+            print('[NG] {} is not found'.format(path))
+            exit(1)
 
-    # Start reading/writing files
-    with open(path, mode='r' , encoding='utf-8',errors='ignore') as input_file:
+        # Determine output file name
+        filename = "VRChat_usrlog.txt"
+        if len(path) > 33:
+            if path[12:32].isalpha() == False:
+                filename = "VRChat_usrlog"+ str(path[12:32]) +".txt"
+        
+        # open output file
+        output_file = open(filename, mode='a', encoding='utf-8', errors='ignore', buffering=1)
 
-        # Read 1 line at a time from the input file
-        for line in input_file:
+        # Start reading/writing files
+        with open(path, mode='r' , encoding='utf-8',errors='ignore') as input_file:
 
-            # If logs of room entry or creation are found.
-            match=re.search('([0-9\.]+ [0-9:]+).+Joining or Creating Room: (.+)',line)
-            if match != None:
-                output_file.write(match.group(1) + " World: " + match.group(2) + "\n")
-            
-            # If logs of room leaving is found
-            match=re.search('([0-9\.]+ [0-9:]+).+\[Behaviour\] Initialized PlayerAPI "(.+)" is remote',line)
-            if match != None:
-                output_file.write(match.group(1) + "  User: " + match.group(2) + "\n")
+            # Read 1 line at a time from the input file
+            for line in input_file:
 
-    # Finish writing to output file
-    output_file.close()
+                # If logs of room entry or creation are found.
+                match=search('([0-9\.]+ [0-9:]+).+Joining or Creating Room: (.+)',line)
+                if match != None:
+                    output_file.write(match.group(1) + " World: " + match.group(2) + "\n")
+                
+                # If logs of room leaving is found
+                match=search('([0-9\.]+ [0-9:]+).+\[Behaviour\] Initialized PlayerAPI "(.+)" is remote',line)
+                if match != None:
+                    output_file.write(match.group(1) + "  User: " + match.group(2) + "\n")
+
+        # Finish writing to output file
+        output_file.close()
+        print("[OK] Conversion End\n     Output file named: "+str(filename))
 
 # Runs only when directly activated
 if __name__ in '__main__':
-    main()
+    main(getArgs())
